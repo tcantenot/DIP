c3eacfa3334584bff446a619bc0a6f418c10be55
Thierry CANTENOT <thierry.cantenot@insa-lyon.fr>
2014-10-29 15:14:11 +0800

    Refactored problem2 program

--- 2/problem2.py
+++ 2/problem2.py
@@ -6,29 +6,29 @@ from PIL import Image
 
 # Smoothing mask A (Figure 3.34 a)
 def smoothing_3x3_mask_a():
-	r = float(1)/9
-	return [[r, r, r], [r, r, r], [r, r, r]]
+    r = float(1)/9
+    return [[r, r, r], [r, r, r], [r, r, r]]
 
 # Smoothing mask B (Figure 3.34 b)
 def smoothing_3x3_mask_b():
-	r = float(1)/16
-	return [[r, 2*r, r], [2*r, 4*r, 2*r], [r, 2*r, r]]
+    r = float(1)/16
+    return [[r, 2*r, r], [2*r, 4*r, 2*r], [r, 2*r, r]]
 
 # Laplacian mask A (Figure 3.42 a)
 def laplacian_3x3_mask_a(A=0):
-	return [[0, -1, 0], [-1, A+4, -1], [0, -1, 0]]
+    return [[0, -1, 0], [-1, A+4, -1], [0, -1, 0]]
 
 # Laplacian mask B (Figure 3.42 b)
 def laplacian_3x3_mask_b(A=0):
-	return [[-1, -1, -1], [-1, A+8, -1], [-1, -1, -1]]
+    return [[-1, -1, -1], [-1, A+8, -1], [-1, -1, -1]]
 
 # Sobel 3x3 mask X-axis
 def sobel_3x3_mask_xaxis():
-	return [[-1, 0, +1], [-2,  0, +2], [-1, 0, +1]]
+    return [[-1, 0, +1], [-2,  0, +2], [-1, 0, +1]]
 
 # Sobel 3x3 mask Y-axis
 def sobel_3x3_mask_yaxis():
-	return [[+1, +2, +1], [0,  0, 0], [-1, -2, -1]]
+    return [[+1, +2, +1], [0,  0, 0], [-1, -2, -1]]
 
 # Transpose a 3x3 mask
 def transpose_mask(mask):
@@ -42,7 +42,7 @@ def transpose_mask(mask):
 
     return tr
 
-# Create and return a new image of size (w, h) from the given linear array of pixels
+# Create and return a image of size (w, h) creted from a linear array of pixels
 def new_image(pixels, w, h):
     image = Image.new("L", [w, h])
     data = image.load()
@@ -52,7 +52,7 @@ def new_image(pixels, w, h):
     return image
 
 
-# Scale the pixels data resulting from the application of the mask to the range [0, 255]
+# Scale the filtered pixels data to the range [0, 255]
 def scale_filtered_data(pixels, w, h):
 
     min_value = min(pixels)
@@ -69,6 +69,7 @@ def scale_filtered_data(pixels, w, h):
 
 
 # Add the filtered data to the original image and scale it to the range [0, 255]
+# Return an linear array of pixels of the sharpened image
 def sharpen_image(image, filtered_data):
 
     w, h = image.size
@@ -89,13 +90,13 @@ def sharpen_image(image, filtered_data):
     return sharpened
 
 
-# Apply a 3x3 mask on an image and return the resulting image
+# Apply a mask on an image and return the resulting image
 def apply_mask(image, mask):
     w, h = image.size
     pixels = list(image.getdata())
 
-	# Get the pixel value at the given bi-dimensional index (x, y)
-	# Return 0 if the if the index falls outside the image
+    # Get the pixel value at the given bi-dimensional index (x, y)
+    # Return 0 if the if the index falls outside the image
     def get_pixel(x, y):
       p = 0 if x<0 or x>(w-1) or y<0 or y>(h-1) else pixels[y*w+x]
       return float(p)
@@ -107,9 +108,6 @@ def apply_mask(image, mask):
 
     mask_w = len(mask)
     mask_h = len(mask[0])
-
-    print mask_w / 2
-    print mask_h / 2
 
     if image.mode == "L": # Grey scale image
         for x in xrange(w):
@@ -125,25 +123,9 @@ def apply_mask(image, mask):
                 data[y*w+x] = v
 
                 # Clamp the pixel value to the range [0, 255]
-                new_pixels[x, y] = max(0, min(255, v))
+                #new_pixels[x, y] = max(0, min(255, v))
 
     return new_image, data
-
-
-def contrast_stretching(image):
-    w, h = image.size
-    pixels = list(image.getdata())
-
-    MP = 255
-    a = min(pixels)
-    b = max(pixels)
-    R = b - a
-
-    for x in xrange(w):
-        for y in xrange(h):
-            pixels[y*w+x] = round(MP * float(pixels[y*w+x]-a)/R)
-
-    return new_image(pixels, w, h)
 
 
 def process_image(image, mask):
@@ -155,53 +137,41 @@ def process_image(image, mask):
 
     scale_filtered_data(filtered_data, w, h)
     scaled_filtered_image = new_image(filtered_data, w, h)
-    #scaled_filtered_image.show()
+    scaled_filtered_image.show()
 
     sharpened = sharpen_image(image, filtered_data)
     sharpened_image = new_image(sharpened, w, h)
     sharpened_image.show()
-
-    #contrast_stretching(scaled_filtered_image).show()
-
-
-# Sum the pixels values of two images and return the resulting image
-def add_images(img1, img2):
-    if img1.size != img2.size: return None
-    if img1.mode != "L" or img2.mode != "L": return None
-
-    w, h = image.size
-    pixels1 = list(img1.getdata())
-    pixels2 = list(img2.getdata())
-
-    new_image = Image.new("L", image.size)
-    new_pixels = new_image.load()
-
-    for x in xrange(w):
-        for y in xrange(h):
-            # Sum the two images' pixels
-            new_pixels[x, y] = pixels1[y*w+x] + pixels2[y*w+x]
-            # Clamp the pixel value to the range [0, 255]
-            new_pixels[x, y] = max(0, min(255, new_pixels[x, y]))
-
-    return new_image
 
 
 # Main
 if __name__ == "__main__":
 
     # Available args
-    parser = argparse.ArgumentParser(description=
-        'Combination of spatial enhancement methods'
+    parser = argparse.ArgumentParser(
+        description= 'Combination of spatial enhancement methods'
     )
 
-    parser.add_argument('path', type=str, help='Image path')
-    parser.add_argument('-a', dest='a', type=float, default=0, help='"A" parameter of Laplacian mask')
+    parser.add_argument('image_path', type=str, help='Image path')
+
+    #mask_types = parser.add_mutually_exclusive_group(required=True)
+    mask_types = parser.add_argument_group('Mask types', 'Type of the mask to apply')
+    mask_types.add_argument('--laplacian', action='store_true', help='Use a 3x3 Laplacian filter \
+        (the value of A can be specified with \'-a\')'
+    )
+    mask_types.add_argument('--sobel', action='store_true', help='Use a 3x3 Sobel filter')
+
+    parser.add_argument('-a', dest='a', type=float, default=0,
+        help='"A" parameter of Laplacian mask'
+    )
 
     # Parse args
     args = parser.parse_args()
 
     # Image path
-    image_path = args.path
+    image_path = args.image_path
+
+    # Mask parameters
     A = args.a
 
     # Check that the image exists
@@ -215,22 +185,24 @@ if __name__ == "__main__":
         print "Failed to open image '{}'".format(image_path)
         sys.exit(-2)
 
+    # Make sure the image is a gray scale image
+    image = image.convert("L")
     image.show()
 
-    # Apply a laplacian B 3x3 mask on the image
-    print "Applying laplacian B 3x3 mask (A = {}) on '{}'...".format(A, image_path)
-    #process_image(image, laplacian_3x3_mask_b(A))
-    process_image(image, laplacian_3x3_mask_b(0.0))
-    process_image(image, laplacian_3x3_mask_b(1.0))
-    process_image(image, laplacian_3x3_mask_b(1.7))
-    #process_image(image, laplacian_3x3_mask_b(2.0))
+    if args.laplacian:
+        # Apply a laplacian B 3x3 mask on the image
+        print "Applying laplacian B 3x3 mask (A = {}) on '{}'...".format(A, image_path)
+        process_image(image, laplacian_3x3_mask_b(A))
 
-    # Apply a Sobel Y-axis 3x3 mask on the image
-    #print "Applying Sobel Y-axis 3x3 mask on '{}'...".format(image_path)
-    #sobel_img, data = apply_mask(image, sobel_3x3_mask_yaxis())
-    #sobel_img.show()
+    if args.sobel:
+        sobel_img = image
 
-    # Apply a Sobel X-axis 3x3 mask on the image
-    #print "Applying Sobel X-axis 3x3 mask on '{}'...".format(image_path)
-    #sobel_img, data = apply_mask(sobel_img, sobel_3x3_mask_xaxis())
-    #sobel_img.show()
+        # Apply a Sobel X-axis 3x3 mask on the image
+        print "Applying Sobel X-axis 3x3 mask on '{}'...".format(image_path)
+        sobel_img, data = apply_mask(sobel_img, transpose_mask(sobel_3x3_mask_xaxis()))
+        sobel_img.show()
+
+        # Apply a Sobel Y-axis 3x3 mask on the image
+        print "Applying Sobel Y-axis 3x3 mask on '{}'...".format(image_path)
+        sobel_img, data = apply_mask(sobel_img, sobel_3x3_mask_yaxis())
+        sobel_img.show()
