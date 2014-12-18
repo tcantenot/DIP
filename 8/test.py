d84a1d4079619fe1882b5d891856a62aa75ef088
Thierry CANTENOT <thierry.cantenot@insa-lyon.fr>
2014-12-19 00:44:32 +0800

    Cleaned up folder 8

--- 8/test.py
+++
@@ -1,9 +0,0 @@
-import scipy
-from scipy import ndimage
-import matplotlib.pyplot as plt
-import numpy as np
-from PIL import Image
-
-im = ndimage.imread('noisy_fingerprint.tif')
-o = ndimage.binary_erosion(im, np.ones((3,3)))
-scipy.misc.imsave('erosion.png', o)
