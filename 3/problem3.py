67a17013673ee0e1b24b415860f7d54b1ade684f
Thierry CANTENOT <thierry.cantenot@insa-lyon.fr>
2014-12-21 20:32:35 +0800

    Updated problem 3

--- 3/problem3.py
+++ 3/problem3.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python
 
-import argparse, os, sys
-from math import exp, pow, sqrt
-from PIL import Image
+import sys
+sys.path.append('..')
+
+import argparse
 import numpy as np
+
+from common import Img
 
 
 # Euclidean distance from the center of the image
 def D(u, v, M, N):
-	return sqrt(pow(u - float(M)/2, 2) + pow(v - float(N)/2, 2))
+	return np.sqrt((u - M/2.)**2 + (v - N/2.)**2)
 
 
 # Low-pass Ideal filter
@@ -27,7 +30,7 @@ def ideal_high_pass(D0, M, N):
 # Low-pass Butterworth filter
 def butterworth_low_pass(D0, n, M, N):
 	def butterworth(u, v):
-		return float(1) / (1 + pow(D(u, v, M, N)/D0, 2*n))
+		return 1. / (1. + np.power(D(u, v, M, N)/D0, 2*n))
 	return butterworth
 
 # High-pass Butterworth filter
@@ -40,7 +43,7 @@ def butterworth_high_pass(D0, n, M, N):
 # Low-pass Gaussian filter
 def gaussian_low_pass(D0, M, N):
 	def gauss(u, v):
-		return exp(-pow(D(u,v,M,N), 2)/(2 * pow(D0, 2)))
+		return exp(-D(u,v,M,N)**2/(2. * D0**2))
 	return gauss
 
 # High-pass Gaussian filter
@@ -52,30 +55,16 @@ def gaussian_high_pass(D0, M, N):
 
 # Apply the filter to the given FFT data
 def apply_filter(fft, filter):
-    return np.asarray([x * filter(i[0], i[1]) for i, x in np.ndenumerate(fft)]).reshape(fft.shape)
+    return np.array([x * filter(u, v) for (u, v), x in np.ndenumerate(fft)]).reshape(fft.shape)
 
 # Preprocessing
-def preprocess(data, w, h):
-    new_data = [0] * w * h
-    for x in xrange(w):
-        for y in xrange(h):
-            idx = y * w + x
-            new_data[idx] = data[idx] * ((-1)**(x+y))
-    return new_data
+def preprocess(data):
+    x, y = np.ogrid[0:data.shape[0], 0:data.shape[1]]
+    return data * ((-1)**(x+y))
 
 # Postprocessing
-def postprocess(data, w, h):
-    return preprocess(data, w, h)
-
-
-# Create and return a new image of size (w, h) from the given linear array of pixels
-def new_image(pixels, w, h):
-    image = Image.new("L", [w, h])
-    data = image.load()
-    for x in xrange(w):
-        for y in xrange(h):
-            data[x, y] = pixels[y*w+x]
-    return image
+def postprocess(data):
+    return preprocess(data)
 
 
 # Main
@@ -97,39 +86,19 @@ if __name__ == "__main__":
     filter_pass_types.add_argument('--low', action='store_true', help='Low pass')
     filter_pass_types.add_argument('--high', action='store_true', help='High pass')
 
-    prepostprocess = parser.add_argument('--npp', action='store_true',
-        help='Disable the pre- and post-processing of the image ((-1)^(x+y))'
-    )
-
     parser.add_argument('-d', dest='d', type=float, default=10, help='"D0" parameter: cutoff frequency')
     parser.add_argument('-n', dest='n', type=float, default=2, help='Butterworth filter order')
 
     # Parse args
     args = parser.parse_args()
 
-    # Image path
-    image_path = args.image_path
-
     # Filter parameters
     D0 = args.d
     n = args.n
 
-    # Check that the image exists
-    if not os.path.isfile(image_path):
-        print "Could not find image '{}'".format(image_path)
-        sys.exit(-1)
-
-    # Open image
-    image = Image.open(image_path)
-    if image == None:
-        print "Failed to open image '{}'".format(image_path)
-        sys.exit(-2)
-
-    # Make sure the image is a gray scale image
-    image = image.convert('L')
-    image.show()
-
-    M, N = image.size
+    # Load image
+    image = Img.load(args.image_path)
+    M, N = image.shape
 
     # Select filter
     filter = None
@@ -152,30 +121,26 @@ if __name__ == "__main__":
         print "No filter selected"
         sys.exit(-3)
 
-    # Image's pixels
-    data = image.getdata()
+    data = image
 
     # Preprocess the data: multiply by (-1)^(x+y)
-    if not args.npp:
-        data = preprocess(data, M, N)
+    data = preprocess(data)
 
     # 2D Fast Fourier Transform
-    fft = np.fft.fft2(np.asarray(data).reshape((M, N)))
+    data = np.fft.fft2(data)
 
     # Apply the selected filter
-    array = apply_filter(fft, filter)
+    data = apply_filter(data, filter)
 
     # 2D Inverse Fast Fourier Transform
-    enhanced_img = np.fft.ifft2(np.asarray(array).reshape((M, N))).ravel()
+    data = np.fft.ifft2(data)
 
     # Retrieve the real part of the IFFT
-    #real_img = [sqrt(x.real**2 + x.imag**2) for x in enhanced_img]
-    real_img = [x.real for x in enhanced_img]
+    data = data.real
 
     # Postprocess the data: multiply by (-1)^(x+y)
-    if not args.npp:
-        real_img = postprocess(real_img, M, N)
+    data = postprocess(data)
 
     # Final image
-    final_img = new_image(real_img, M, N)
-    final_img.show()
+    final_img = data
+    Img.show(final_img)
