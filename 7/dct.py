b60ffab5d628856f80a4c559dd34e1c2a7c397ce
Thierry CANTENOT <thierry.cantenot@insa-lyon.fr>
2014-12-23 18:42:49 +0800

    Added problem 7 to latex (DCT) + updated code

--- 7/dct.py
+++ 7/dct.py
@@ -1,5 +1,13 @@
+#!/usr/bin/python
+
+import sys
+sys.path.append('..')
+
+import argparse
 import numpy as np
 from scipy import fftpack
+
+from common import Img
 
 # Textbook p598: Zonal mask
 def zonal_mask(n, s=8):
@@ -104,3 +112,36 @@ def dct_compression(img, mask):
     M, N = img.shape
     chunks = compress_image(img, mask)
     return reconstruct_image(chunks, M, N)
+
+
+# Main
+if __name__ == "__main__":
+
+    # Available args
+    parser = argparse.ArgumentParser(description='Transform image compression')
+
+    parser.add_argument('image_path', type=str, help='Image path')
+
+    dct_masks = parser.add_mutually_exclusive_group()
+    dct_masks.add_argument('--zonal', action='store_true', help='Use a zonal mask')
+    dct_masks.add_argument('--threshold', action='store_true', help='Use a threshold mask')
+
+    parser.add_argument('-z', dest='z', type=int, default=4, help='Zonal mask size')
+
+    # Parse args
+    args = parser.parse_args()
+
+    # Input image
+    img = Img.load(args.image_path, dtype=np.float)
+    Img.show(img)
+
+    mask = None
+    if args.zonal:
+        mask = zonal_mask(args.z)
+    elif args.threshold:
+        mask = threshold_mask()
+
+    if mask is not None:
+        compressed = dct_compression(img, mask)
+        Img.show(compressed)
+        Img.show(img - compressed) # Difference between original and compressed
